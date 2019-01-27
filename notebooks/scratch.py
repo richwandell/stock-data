31fadc9cda93c7e6c7803294a53d22c2473cc802
Rich Wandell <richwandell@gmail.com>
2019-01-27 18:48:25 -0500

    updates

--- notebooks/scratch.py
+++ notebooks/scratch.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     years = mdates.YearLocator()  # every year
     months = mdates.MonthLocator()  # every month
     days = mdates.DayLocator()
-    yearsFmt = mdates.DateFormatter('%Y')
+    yearsFmt = mdates.DateFormatter('%Y-%m-%d')
 
     db = Db("../cache")
     df = db.get_symbols_as_dataframe(['AAPL'])
@@ -36,15 +36,26 @@ if __name__ == '__main__':
     signal_line = signal_line.reset_index()
     macd_hist = macd_line['adjusted_close'] - signal_line['adjusted_close']
 
+    derivatives = np.diff(macd_hist) / np.diff(ewmdf['unix_time'])
+    # signals = []
+    # for i, hist in enumerate(macd_hist):
+    #     if i > 0:
+    #         #buy signal
+    #         if 0 > macd_hist[i-1]
+    #     print(i, hist)
+
+
     scatter_data = ewmdf[['unix_time', 'adjusted_close', 'date_time']].as_matrix()
+
+    plt.style.use('ggplot')
     fig = plt.figure()
     ax = fig.add_subplot(211)
     ax.scatter(scatter_data[:, 2], scatter_data[:, 1].tolist(), label='Price')
     ax.plot(days_12['date_time'], days_12['adjusted_close'], label='15 Days Moving Average', color='red')
     ax.plot(days_26['date_time'], days_26['adjusted_close'], label='30 Days Moving Average', color='green')
-    ax.xaxis.set_major_locator(years)
+    ax.xaxis.set_major_locator(months)
+    ax.xaxis.set_minor_locator(days)
     ax.xaxis.set_major_formatter(yearsFmt)
-    ax.xaxis.set_minor_locator(months)
     ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
 
     plt.title('MACD indicator')
@@ -55,9 +66,12 @@ if __name__ == '__main__':
     ax.plot(days_12['date_time'], signal_line['adjusted_close'], label='Signal Line', color='red')
     colors = ["green" if x > 0 else "red" for x in macd_hist]
     ax.bar(scatter_data[:, 2], macd_hist, label='MACD Hist', color=colors)
-    ax.xaxis.set_major_locator(years)
+
+    ax.plot(days_12['date_time'].as_matrix()[0:249], derivatives.tolist())
+
+    ax.xaxis.set_major_locator(months)
+    ax.xaxis.set_minor_locator(days)
     ax.xaxis.set_major_formatter(yearsFmt)
-    ax.xaxis.set_minor_locator(months)
     ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
     ax.axhline(0)
 
